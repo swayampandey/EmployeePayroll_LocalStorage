b019bbc494ee6a6ed041cef775dca95fa824b49c
Swayam Prakash <swayam265@gmail.com>
2022-10-02 21:54:23 +0530

    [Swayam] : added functionality to reset form in employee form in employee payroll app local storage

--- js/EmployeePayroll.js
+++ js/EmployeePayroll.js
@@ -112,6 +112,7 @@ function save() {
     try {
 
         let employeePayroll = new EmployeePayrollData(empName, empProfileImage, empGender, empDepartmentArr, empSalary, empStartDate, empNotes);
+
         saveToLocalStorage(employeePayroll);
 
 
@@ -138,4 +139,29 @@ function getFromLocalStorage() {
     for (let emp of empPayrollDataList) {
         console.log(emp);
     }
+}
+
+const resetForm = () => {
+    console.log("reset called ")
+    setValue('#name', '');
+    unsetSelectedValues('[name=profile]');
+    unsetSelectedValues('[name=gender]');
+    unsetSelectedValues('[name=department]');
+    setValue('#salary', '');
+    setValue('#notes', '');
+    setValue('#day', '1');
+    setValue('#month', '0');
+    setValue('#year', '2022');
+}
+
+const unsetSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach(item => {
+        item.checked = false;
+    });
+}
+
+const setValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.value = value;
 }
